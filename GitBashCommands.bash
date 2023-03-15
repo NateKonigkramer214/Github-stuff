03deeb9a8c420ac65de86ca8d39b366808e6f7d5
Nathan Konigkramer <98358837+NateKonigkramer214@users.noreply.github.com>
2023-03-15 13:24:26 +1300

    Notes

---
+++ GitBashCommands.bash
@@ -0,0 +1,23 @@
+#GitHub Commands
+
+#Git add file for commit
+
+git add <filename>
+
+#Git commit files
+
+git commit 
+
+#Move around branchs 
+
+git checkout <branchname>
+
+#pull changes
+
+git pull
+
+#push changes to branch/main
+
+git push
+
+
---
+++ githubpages.bash
@@ -0,0 +1,10 @@
+#GitHub Pages
+
+#Page must be named:
+
+<user>.github.io
+
+#Select Initialize this repository with a README.
+
+#Click Create repository.
+
